5a2e7bb301258c83e84e62791877aef682ff8a1d
jenkins-metasploit <jenkins-metasploit@build-production.r7ops.com>
2026-04-30 23:06:36 +0000

    Bump version of framework to 6.4.132

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -32,7 +32,7 @@ module Metasploit
         end
       end
 
-      VERSION = "6.4.131"
+      VERSION = "6.4.132"
       MAJOR, MINOR, PATCH = VERSION.split('.').map { |x| x.to_i }
       PRERELEASE = 'dev'
       HASH = get_hash
